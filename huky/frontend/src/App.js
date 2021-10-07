329cde35d24462a6ec704515120d74a20f257e21
nashemistoart@gmail.com <nashemistoart@gmail.com>
2021-10-07 14:39:36 +0300

    useState,UseEfect

--- huky/frontend/src/App.js
+++ huky/frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState,useEffect} from 'react'
 import TodoList from './TodoList'
 
 export default function App ()  {
@@ -10,11 +10,27 @@ export default function App ()  {
   // }
 
   const[todos, setTodos]=useState([
-    {id: 1, title: 'First task', completed: false},
-      {id: 2, title: 'Second task', completed: true},
+    // {id: 1, title: 'First task', completed: false},
+    //   {id: 2, title: 'Second task', completed: true},
+
+
   ])
   
   const[todoTitle,setTodoTitle]=useState('')
+
+// useEffect(()=>{
+//   const row=localStorage.getItem('todos')||[]
+//   setTodos(JSON.parse(raw))
+// },[])
+const handleClick=()=>console.log('click')
+useEffect(()=>{
+  document.addEventListener('click',handleClick)
+  localStorage.setItem('todos',JSON.stringify([todos]))
+  return()=>{
+    document.removeEventListener('click',handleClick)
+  }
+},[todos])
+
 
   const addTodo=event=>{
 if(event.key==='Enter'){
